d97c181f15924f2dda6f9ae59bdfeb7bc0ac2e03
AhmedAmrNabil <ahmedamr24680@gmail.com>
2024-03-30 18:31:17 +0200

    added soldiers and earth gunner

--- Armies/AlienArmy.h
+++ Armies/AlienArmy.h
@@ -1,3 +1,7 @@
-class AlienArmy {
+#include "Army.h"
+#include "../BattleUnits/Soldier.h"
 
+class AlienArmy:public Army {
+   public:
+    bool getDronePair(Unit* drone1,Unit* drone2);
 };
--- Armies/Army.h
+++ Armies/Army.h
@@ -1,8 +1,7 @@
 #include <iostream>
 #include "LinkedQueue.h"
-#include "Stack.h"
-#include "EarthSoldier.h"
-#include "AlienSoldier.h"
+// #include "Stack.h"
+#include "Soldier.h"
 #include "EarthTank.h"
 #include "EarthGunner.h"
 #include "AlienMonster.h"
@@ -10,10 +9,14 @@
 using namespace std;
 class Army {
 private:
-	LinkedQueue <Soldiers> ;
+	// LinkedQueue <Soldier> soldiers;
 
 public:
-
-
+	virtual bool addUnit(UNIT_TYPE type, int joinTime, int health, int power, int attackCapacity);
+	virtual bool addUnit(UNIT_TYPE type,Unit* unit);
+	bool getSoldier(Unit* alienSoldier);
+    void addToKilled(Unit* killedUnit);
+	void addAttacked(UNIT_TYPE type,Unit* attackedUnit);
+	
 
 };
