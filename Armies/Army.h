98964082e97aca82751e74e415cc197ae4348de1
Youssefwafa7 <149392734+Youssefwafa7@users.noreply.github.com>
2024-03-30 16:49:47 +0200

    Class names (#6)
    * dequeue fix
    
    * added classnames

---
+++ Armies/Army.h
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "LinkedQueue.h"
+#include "Stack.h"
+#include "EarthSoldier.h"
+#include "AlienSoldier.h"
+#include "EarthTank.h"
+#include "EarthGunner.h"
+#include "AlienMonster.h"
+#include "AlienDrone.h"
+using namespace std;
+class Army {
+private:
+	LinkedQueue <Soldiers> ;
+
+public:
+
+
+
+};
