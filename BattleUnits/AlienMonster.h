621ff789f854ef6dfa3960fcde12e69f1309b4ec
Ahmed Amr <43810060+AhmedAmrNabil@users.noreply.github.com>
2024-03-31 19:58:34 +0200

    removed safan

--- BattleUnits/AlienMonster.h
+++ BattleUnits/AlienMonster.h
@@ -6,7 +6,7 @@ public:
 	AlienMonster(int joinTime, int health, int power, int attackCapacity) :
 		Unit(AM, joinTime, health, power, attackCapacity)
 	{
-		int safan = 8;
+		
 	}
 	void attack(Army* enemyArmy, int timestep) 
 	{
