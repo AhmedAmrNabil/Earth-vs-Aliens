fe8d3c7662bfb93fe29d2d0411e1ec5a36166e3e
Abdallah Farag <146781191+Abdallah-farag27@users.noreply.github.com>
2024-04-01 19:02:47 +0200

    Alien Monster Done/Alien Army done except Drones (#16)
    * Alien Monster Done/Alien Army done except Drones
    
    * solving wafa issues

--- BattleUnits/AlienMonster.h
+++ BattleUnits/AlienMonster.h
@@ -10,17 +10,36 @@ class AlienMonster : public Unit {
     AlienMonster(int joinTime, int health, int power, int attackCapacity) : Unit(AM, joinTime, health, power, attackCapacity) {
     }
     void attack(Army* enemyArmy, int timestep) {
-        LinkedQueue<Soldier*> tmpList;
-        LinkedListStack<Unit*> EarthTanks;
+        LinkedQueue<Unit*> tempSoldiers;
+        LinkedListStack<Unit*> tempEarthTanks;
         Unit* T1 = nullptr;
         Unit* S1 = nullptr;
 		Unit* tmp;
-        for (int i = 0; i < getAttackCapacity(); ++i) {
-                enemyArmy->getUnit(ET,T1,tmp);
-                T1->getAttacked(this, timestep);
-                enemyArmy->getUnit(S,S1,tmp);
-                S1->getAttacked(this, timestep);
-            
+        for (int i = 0; i < getAttackCapacity(); ++i)
+        {
+           enemyArmy->getUnit(ET,T1,tmp);
+           T1->getAttacked(this, timestep);
+           enemyArmy->getUnit(S,S1,tmp);
+           S1->getAttacked(this, timestep);
+           if (S1->isDead())
+               enemyArmy->addToKilled(S1);
+           else
+               tempSoldiers.enqueue(S1);
+           if (T1->isDead())
+               enemyArmy->addToKilled(T1);
+           else
+               tempEarthTanks.push(T1);
+        }
+        Unit* temp = nullptr;
+        while (!tempSoldiers.isEmpty())
+        {
+            tempSoldiers.dequeue(temp);
+            enemyArmy->addExisting(S, temp);
+        }
+        while (!tempEarthTanks.isEmpty()) 
+        {
+            tempEarthTanks.pop(temp);
+            enemyArmy->addExisting(ET, temp);
         }
     }
 };
