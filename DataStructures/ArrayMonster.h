fa43a561780dbcaf0ccd1b26c37ab2725c7eeb22
abdallah farag <abdallah_altalbawy@yahoo.com>
2024-04-19 04:55:56 +0200

    peek function done in AA

--- DataStructures/ArrayMonster.h
+++ DataStructures/ArrayMonster.h
@@ -1,9 +1,5 @@
 #pragma once
 #include "../BattleUnits/AlienMonster.h"
-#include <cmath>
-#include <cstdlib> 
-#include <time.h> 
-
 
 class ArrayMonster
 {
@@ -11,44 +7,13 @@ private:
 	Unit* arrayMonster[1000];
 	int monsterCount;
 public:
-	ArrayMonster() :arrayMonster() {
-		srand(time(0));
-		monsterCount = 0;
-	}
 
-	bool isEmpty() {
-		return (monsterCount == 0);
-	}
-
-	bool isFull() {
-		return (monsterCount == 1000);
-	}
-
-	bool insert(Unit* a1) {
-		if (!isFull()) {
-			arrayMonster[monsterCount++] = a1;
-			return true;
-		}
-		return false;
-	}
-
-	bool pick(Unit*& AM) {
-		if (!isEmpty()) {
-			int random = rand() % monsterCount;
-			AM = arrayMonster[random];
-			arrayMonster[random] = arrayMonster[--monsterCount];
-			return true;
-		}
-		return false;
-	}
-
-	void print() {
-		if (isEmpty()) return;
-		cout << "[" << arrayMonster[0];
-		for (int i = 1; i < monsterCount; i++) {
-			cout << ", " << arrayMonster[i];
-		}
-		cout << "]";
-	}
+	ArrayMonster();
+	bool isEmpty();
+	bool isFull();
+	bool insert(Unit* a1);
+	bool pick(Unit*& AM);
+	bool peek(Unit*& AM);
+	void print();
 	int getCount() { return monsterCount; }
 };
